41247ea1e99a2b38e51ee7acc557c77971b6e0e3
Aubrey Nicoll <aubrey.nicoll@gmail.com>
2021-02-27 15:03:46 +1000

    serving static mp3 works with hardcoded url

--- server/routes/audioRouter.js
+++ server/routes/audioRouter.js
@@ -1,18 +1,8 @@
 const audioRouter = require('express').Router()
-const fs = require('fs')
+const express = require('express')
+// const fs = require('fs')
 const path = require('path')
 
-audioRouter.get('/', (req, res) => {
-  const filePath = path.join(__dirname, '..', '/temp/a.mp3')
-  const stat = fs.statSync(filePath)
-  const readStream = fs.createReadStream(filePath)
-
-  res.writeHead(200, {
-    'Content-Type': 'audio/mpeg',
-    'Content-Length': stat.size,
-  })
-
-  readStream.pipe(res)
-})
+audioRouter.use(express.static(path.join(__dirname, '../temp')))
 
 module.exports = audioRouter
